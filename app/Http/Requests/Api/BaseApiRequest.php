ea652deb70733e7729a3d26a2dc03c0c34239406
MOEUAED <ziyanimouad@gmail.com>
2026-03-12 14:00:37 +0000

    chore: add app/Http/Requests/Api/BaseApiRequest.php

---
+++ app/Http/Requests/Api/BaseApiRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseApiRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+                'message' => 'Erreur de validation',
+            ], 422)
+        );
+    }
+}
