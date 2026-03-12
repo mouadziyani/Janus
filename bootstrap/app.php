83e4f3e0c172e5dbb2ad78d00f7ad8aa38d5a10b
MOEUAED <ziyanimouad@gmail.com>
2026-03-12 14:00:36 +0000

    chore: add bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +20,27 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'errors' => null,
+                'message' => 'Unauthorized',
+            ], 401);
+        });
+
+        $exceptions->render(function (AuthorizationException $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'errors' => null,
+                'message' => 'Forbidden',
+            ], 403);
+        });
+
+        $exceptions->render(function (ModelNotFoundException|NotFoundHttpException $e, Request $request) {
+            return response()->json([
+                'success' => false,
+                'errors' => null,
+                'message' => 'Not found',
+            ], 404);
+        });
     })->create();
